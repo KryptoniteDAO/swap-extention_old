97b919379c002b38301e66eb1c59d8155ea72049
Carson.Xyg <carson.xyg@gmail.com>
2023-05-24 19:39:21 +0800

    add scripts & schema

---
+++ scripts/build_schema.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
+
+#for c in "$projectPath"/contracts/*; do
+#  (cd $c && cargo schema)
+#done
+cargo schema
